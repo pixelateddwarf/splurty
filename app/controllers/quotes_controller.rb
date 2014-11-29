5d1c9c21ca263b0850f92d1c7356bd74094a9e6d
pixelateddwarf <pixelateddwarf@gmail.com>
2014-11-28 23:51:57 -0500

    Huston ,,, We have a modal

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,10 +5,6 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first
   end
 
-  def new
-    @quote = Quote.new
-  end
-
   def create 
     @quote = Quote.create(quote_params)
     if @quote.invalid?
